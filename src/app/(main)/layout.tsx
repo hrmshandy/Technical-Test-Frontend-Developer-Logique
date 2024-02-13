c34b92b2bf3318d61e426c25553fdc1ab291099d
Shandy Hermawan <hrm.shandy05@gmail.com>
2024-02-13 19:40:31 +0700

    fix: lint in main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -8,8 +8,7 @@ export default function MainLayout({
   return (
     <div className="relative min-h-screen bg-slate-50">
       <Header />
-        <main className="pt-32">
-        {children}</main>
+      <main className="pt-32">{children}</main>
     </div>
   );
 }
