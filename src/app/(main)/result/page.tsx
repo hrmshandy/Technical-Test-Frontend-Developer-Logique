2181a4476c5d589a86a948e200f5795d66d77c7d
Shandy Hermawan <hrm.shandy05@gmail.com>
2024-02-13 19:40:52 +0700

    feat: add suspend in result page

--- src/app/(main)/result/page.tsx
+++ src/app/(main)/result/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from 'react';
+
 import SearchResult from '@/views/search-result/search-result';
 
 export default function Result() {
-  return <SearchResult />;
+  return (
+    <Suspense>
+      <SearchResult />
+    </Suspense>
+  );
 }
